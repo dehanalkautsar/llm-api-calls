7b1693aeaf1542fd45a3da772e4b3185b166e62f
rayendito <13519157@std.stei.itb.ac.id>
2025-01-15 17:01:13 +0400

    add vllm chat examples and slurm files to gitignore

--- vllm_try.py
+++ vllm_try.py
@@ -1,10 +1,36 @@
 from vllm import LLM, SamplingParams
 
 prompts = [
-    "The capital of France is",
-    "The capital of Ireland is",
-    "The capital of Poland is",
+    [
+        {
+            "role" : "user",
+            "content" : "What is the capital of Ireland"
+        },
+        {
+            "role" : "assistant",
+            "content" : "Dublin"
+        },
+        {
+            "role" : "user",
+            "content" : "What is the currency?"
+        },
+    ],
+        [
+        {
+            "role" : "user",
+            "content" : "What is the capital of Japan"
+        },
+        {
+            "role" : "assistant",
+            "content" : "Tokyo"
+        },
+        {
+            "role" : "user",
+            "content" : "What is the currency?"
+        },
+    ],
 ]
+
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 llm = LLM(model="meta-llama/Llama-3.2-1B")
